8a6c6fcdffde07362fdab313ad3485db37dfcdda
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-06-02 15:43:20 +0200

    fix: remove underline text on link hover

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,7 +11,7 @@ export default function HomepageFeatures() {
       <div className="container">
         <div className="flex flex-col md:flex-row items-center gap-[40px] mt-[80px] mb-[40px]">
           <div className="flex-1">
-            <Link to="/intro">
+            <Link to="/intro" className={'hover:no-underline'}>
               <BigCard
                 title="Quick start"
                 description="Deploy your own node, setup your testnet and more."
@@ -20,7 +20,7 @@ export default function HomepageFeatures() {
             </Link>
           </div>
           <div className="flex-1">
-            <Link to="/validators">
+            <Link to="/validators" className={'hover:no-underline'}>
               <BigCard
                 title="Guides"
                 description="Follow guides to using popular Ethereum tools with HAQQ."
