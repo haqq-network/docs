291f5580b7bc6b79f2a88e5e0b2871a823c82f40
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-06-02 15:43:54 +0200

    fix: used small cards, add dark mode styles

--- src/components/explore-block/explore-block.tsx
+++ src/components/explore-block/explore-block.tsx
@@ -4,36 +4,52 @@ import { SmallCard } from '../small-card/small-card';
 import { IntroducionIcon } from '../icons/introduciton-icon';
 import { BasicsIcon } from '../icons/basics-icon';
 import { CoreConceptsIcon } from '../icons/core-concepts-icon';
+import Link from '@docusaurus/Link';
+import clsx from 'clsx';
 
 export function ExploreBlock() {
   return (
-    <div className={styles.wrapper}>
-      <h2 className="text-[24px] font-bold leading-[2rem] md:text-[32px] md:leading-[36px] md:tracking-[.01em]">
+    <div
+      className={clsx(
+        styles.wrapper,
+        'relative pt-[72px] px-[16px] pb-[16px] bg-white rounded-[8px] md:rounded-none md:bg-transparent sm:mt-[40px] md:px-0 md:pb-0',
+      )}
+    >
+      <div className="text-[24px] font-bold leading-[2rem] md:text-[32px] md:leading-[36px] md:tracking-[.01em]">
         Explore Haqq
-      </h2>
+      </div>
       <p className="mt-[12px]">
         Get familiar with Haqq and explore its main concepts.
       </p>
-      <div className={styles.sections}>
-        <SmallCard
-          title="Introducion"
-          description="Read a high-level overview of Haqq and its architecture."
-        >
-          <IntroducionIcon />
-        </SmallCard>
-        <SmallCard
-          title="Basics"
-          description="Start with the basic concepts of Haqq, like accounts and transactions."
-        >
-          <BasicsIcon />
-        </SmallCard>
-        <SmallCard
-          title="Core Concepts"
-          description="Read about the core concepts like encoding and events."
-          className="max-[640px]:border-b-0"
-        >
-          <CoreConceptsIcon />
-        </SmallCard>
+      <div
+        className={
+          'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:gap-[12px] md:gap-[24px] mt-[32px] mx-[-16px] sm:mx-0 md:mt-[48px] md:mb-[80px]'
+        }
+      >
+        <Link to={`/intro`} className="hover:no-underline hover:text-inherit">
+          <SmallCard
+            title="Introducion"
+            description="Read a high-level overview of Haqq and its architecture."
+            icon={<IntroducionIcon />}
+          />
+        </Link>
+
+        <Link to={`/intro`} className="hover:no-underline hover:text-inherit">
+          <SmallCard
+            title="Basics"
+            description="Start with the basic concepts of Haqq, like accounts and transactions."
+            icon={<BasicsIcon />}
+          />
+        </Link>
+
+        <Link to={`/intro`} className="hover:no-underline hover:text-inherit">
+          <SmallCard
+            title="Core Concepts"
+            description="Read about the core concepts like encoding and events."
+            className="max-[640px]:border-b-0"
+            icon={<CoreConceptsIcon />}
+          />
+        </Link>
       </div>
     </div>
   );
