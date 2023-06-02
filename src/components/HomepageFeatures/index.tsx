60863ee8eaf04fdc659cdc6ff48c23cb9b842d89
Oleg Shilov <me@olegshilov.com>
2023-06-02 16:00:30 +0700

    wip

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -9,88 +9,33 @@ import { IntroducionIcon } from '../icons/introduciton-icon';
 import { BasicsIcon } from '../icons/basics-icon';
 import { CoreConceptsIcon } from '../icons/core-concepts-icon';
 import { ExploreBlock } from '../explore-block/explore-block';
-
-type FeatureItem = {
-  title: string;
-  Svg: ComponentType<ComponentProps<'svg'>>;
-  description: JSX.Element;
-};
-
-const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({ Svg, title, description }) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
+import Link from '@docusaurus/Link';
 
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
         <div className="flex flex-col md:flex-row items-center gap-[40px] mt-[40px]">
-          <BigCard
-            link="intro"
-            type="read"
-            title="Quick start"
-            description="Deploy your own node, setup your testnet and more."
-          >
-            <RocketIcon />
-          </BigCard>
-
-          <BigCard
-            link="validators"
-            type="use"
-            title="Guides"
-            description="Follow guides to using popular Ethereum tools with HAQQ."
-          >
-            <CodeIcon />
-          </BigCard>
+          <div className="flex-1">
+            <Link to="/intro">
+              <BigCard
+                title="Quick start"
+                description="Deploy your own node, setup your testnet and more."
+                icon={<RocketIcon />}
+              />
+            </Link>
+          </div>
+          <div className="flex-1">
+            <Link to="/validators">
+              <BigCard
+                title="Guides"
+                description="Follow guides to using popular Ethereum tools with HAQQ."
+                icon={<CodeIcon />}
+              />
+            </Link>
+          </div>
         </div>
+
         <ExploreBlock />
       </div>
     </section>
--- src/components/big-card/big-card.tsx
+++ src/components/big-card/big-card.tsx
@@ -1,42 +1,30 @@
-import React, { PropsWithChildren, ReactNode } from 'react';
-import Link from '@docusaurus/Link';
-import styles from './big-card.module.css';
+import React, { ReactNode } from 'react';
 import clsx from 'clsx';
 
-export interface BigCardProps {
-  type?: 'read' | 'use';
-  title: string;
-  description?: string;
-  className?: string;
-  link?: string;
-}
-
 export function BigCard({
-  type,
   title,
   description,
   className,
-  link,
-  children,
-}: PropsWithChildren<BigCardProps>) {
+  icon,
+}: {
+  icon: ReactNode;
+  title: string;
+  description: string;
+  className?: string;
+}) {
   return (
-    <Link to={`/${link}`} className="hover:no-underline hover:text-inherit">
-      <div className={clsx(styles.big__card, className)}>
-        <div className="flex items-center justify-center self-center max-h-[144px] sm:h-auto pt-[1rem] sm:pt-0">
-          {children}
+    <div className={clsx('flex flex-row w-full', className)}>
+      <div className="flex items-center justify-center self-center max-h-[144px] sm:h-auto pt-[1rem] sm:pt-0">
+        {icon}
+      </div>
+      <div className="flex-1 p-[24px] sm:pt-[28px] sm:px-[32px] sm:pb-[32px] flex flex-col">
+        <div className="text-[14px] tracking-[.01em] leading-[1.75em] font-semibold mt-[4px]">
+          {title}
         </div>
-        <div className="flex-1 p-[24px] sm:pt-[28px] sm:px-[32px] sm:pb-[32px] flex flex-col">
-          <h2 className="text-[12px] tracking-[.2em] uppercase text-dim">
-            {type}
-          </h2>
-          <h1 className="text-[14px] tracking-[.01em] leading-[1.75em] font-semibold mt-[4px] ">
-            {title}
-          </h1>
-          <p className="text-[14px] tracking-[.03em] leading-[1.25em] mt-[12px]">
-            {description}
-          </p>
+        <div className="text-[14px] tracking-[.03em] leading-[1.25em] mt-[12px]">
+          {description}
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
