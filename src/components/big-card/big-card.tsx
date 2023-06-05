b80897ce5ffa3ca9d0f0633fc1559dd365ed4b79
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-06-05 14:59:02 +0200

    fix: remove big cards module css

--- src/components/big-card/big-card.tsx
+++ src/components/big-card/big-card.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from 'react';
 import clsx from 'clsx';
-import styles from './big-card.module.css';
 
 export function BigCard({
   title,
@@ -16,12 +15,11 @@ export function BigCard({
   return (
     <div
       className={clsx(
-        'flex flex-col md:flex-row w-full rounded-md shadow hover:shadow-lg border border-gray/20',
+        'flex flex-col md:flex-row w-full rounded-md shadow border border-gray/20 text-dim',
         'transition-all duration-[250ms] ease-in-out',
-        'hover:translate-y-[-3px]',
+        'hover:translate-y-[-3px] hover:shadow-lg',
         'md:min-h-[184px]',
         'bg-gradient-to-r from-white via-transparent to-[rgba(236,87,40,0.3)]',
-        styles.big__card,
         className,
       )}
     >
@@ -29,7 +27,7 @@ export function BigCard({
         {icon}
       </div>
       <div className="flex-1 p-[24px] sm:pt-[28px] sm:px-[32px] sm:pb-[32px] flex flex-col">
-        <div className="text-[14px] tracking-[.01em] leading-[1.75em] font-semibold mt-[4px]">
+        <div className="text-[20px] tracking-[.01em] leading-[1.75em] font-semibold mt-[4px] text-black">
           {title}
         </div>
         <div className="text-[14px] tracking-[.03em] leading-[1.25em] mt-[12px]">
