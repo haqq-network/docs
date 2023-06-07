e2b5f78c333dcb59ed7ec2208c056f956e2a3961
Jihad HadjHusein <skiptomylou999@gmail.com>
2023-06-07 13:29:59 +0200

    fix: add chain types

--- src/utils/chain-config.ts
+++ src/utils/chain-config.ts
@@ -1,9 +1,61 @@
 import Swal from 'sweetalert2';
 
-export const chainConfig = {
+const MAINNET_ID = 'haqq_11235-1';
+const TESTNET_ID = 'haqq_54211-3';
+const HAQQ_COIN_DENOM = 'ISLM';
+const HAQQ_COIN_MINIMAL_DENOM = 'aISLM';
+
+type HAQQChainIds = typeof MAINNET_ID | typeof TESTNET_ID;
+
+interface ChainConfig {
+  chainId: HAQQChainIds;
+  chainName: string;
+  rpc: string;
+  rest: string;
+  features: string[];
   stakeCurrency: {
-    coinDenom: 'ISLM',
-    coinMinimalDenom: 'aISLM',
+    coinDenom: string;
+    coinMinimalDenom: string;
+    coinDecimals: number;
+  };
+  bech32Config: {
+    bech32PrefixAccAddr: string;
+    bech32PrefixAccPub: string;
+    bech32PrefixValAddr: string;
+    bech32PrefixValPub: string;
+    bech32PrefixConsAddr: string;
+    bech32PrefixConsPub: string;
+  };
+  currencies: Currency[];
+  feeCurrencies: FeeCurrency[];
+  bip44: {
+    coinType: number;
+  };
+}
+
+interface Currency {
+  coinDenom: string;
+  coinMinimalDenom: string;
+  coinDecimals: number;
+}
+
+interface FeeCurrency extends Currency {
+  gasPriceStep: {
+    low: number;
+    average: number;
+    high: number;
+  };
+}
+
+export const MainnetConfig: ChainConfig = {
+  chainId: MAINNET_ID,
+  chainName: 'HAQQ Mainnet',
+  rpc: 'https://rpc.tm.haqq.network',
+  rest: 'https://rest.cosmos.haqq.network',
+  features: ['ibc-transfer', 'ibc-go', 'eth-address-gen', 'eth-key-sign'],
+  stakeCurrency: {
+    coinDenom: HAQQ_COIN_DENOM,
+    coinMinimalDenom: HAQQ_COIN_MINIMAL_DENOM,
     coinDecimals: 18,
   },
   bech32Config: {
@@ -16,15 +68,15 @@ export const chainConfig = {
   },
   currencies: [
     {
-      coinDenom: 'ISLM',
-      coinMinimalDenom: 'aISLM',
+      coinDenom: HAQQ_COIN_DENOM,
+      coinMinimalDenom: HAQQ_COIN_MINIMAL_DENOM,
       coinDecimals: 18,
     },
   ],
   feeCurrencies: [
     {
-      coinDenom: 'ISLM',
-      coinMinimalDenom: 'aISLM',
+      coinDenom: HAQQ_COIN_DENOM,
+      coinMinimalDenom: HAQQ_COIN_MINIMAL_DENOM,
       coinDecimals: 18,
       gasPriceStep: {
         low: 10000000000,
@@ -36,27 +88,65 @@ export const chainConfig = {
   bip44: {
     coinType: 60,
   },
-  chainId: 'haqq_11235-1',
-  chainName: 'Haqq Network',
+};
+
+export const TestnetConfig: ChainConfig = {
+  chainId: TESTNET_ID,
+  chainName: 'HAQQ Testnet',
   rpc: 'https://rpc.tm.haqq.network',
   rest: 'https://rest.cosmos.haqq.network',
   features: ['ibc-transfer', 'ibc-go', 'eth-address-gen', 'eth-key-sign'],
+  stakeCurrency: {
+    coinDenom: HAQQ_COIN_DENOM,
+    coinMinimalDenom: HAQQ_COIN_MINIMAL_DENOM,
+    coinDecimals: 18,
+  },
+  bech32Config: {
+    bech32PrefixAccAddr: 'haqq',
+    bech32PrefixAccPub: 'haqq' + 'pub',
+    bech32PrefixValAddr: 'haqq' + 'valoper',
+    bech32PrefixValPub: 'haqq' + 'valoperpub',
+    bech32PrefixConsAddr: 'haqq' + 'valcons',
+    bech32PrefixConsPub: 'haqq' + 'valconspub',
+  },
+  currencies: [
+    {
+      coinDenom: HAQQ_COIN_DENOM,
+      coinMinimalDenom: HAQQ_COIN_MINIMAL_DENOM,
+      coinDecimals: 18,
+    },
+  ],
+  feeCurrencies: [
+    {
+      coinDenom: HAQQ_COIN_DENOM,
+      coinMinimalDenom: HAQQ_COIN_MINIMAL_DENOM,
+      coinDecimals: 18,
+      gasPriceStep: {
+        low: 10000000000,
+        average: 25000000000,
+        high: 40000000000,
+      },
+    },
+  ],
+  bip44: {
+    coinType: 60,
+  },
 };
 
-export const handler = async () => {
+export const handler = async (config: ChainConfig) => {
   if ((window as any).keplr) {
     try {
-      await (window as any).keplr.enable(chainConfig.chainId);
+      await (window as any).keplr.enable(config.chainId);
       Swal.fire({
         title: 'Note',
-        text: `Testnet ${chainConfig.chainId} has already added`,
+        text: `Testnet ${config.chainId} has already added`,
         icon: 'info',
         confirmButtonText: 'Awesome',
       });
     } catch (e) {
       try {
-        await (window as any).keplr.experimentalSuggestChain(chainConfig);
-        await (window as any).keplr.enable(chainConfig.chainId);
+        await (window as any).keplr.experimentalSuggestChain(config);
+        await (window as any).keplr.enable(config.chainId);
       } catch (e2) {
         Swal.fire({
           title: 'Error',
