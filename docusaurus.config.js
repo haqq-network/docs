f77953c12a2791673eecb68de3f4b00ebca5cbb1
Evgeniy Abramov <i@kio.ninja>
2024-02-09 21:20:04 +0400

    chore: try update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  trailingSlash: false,
+  trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
