7c957b60b55f18f2ceb41018f1e0a0dabb258d29
garageinc <9-b-rinat@rambler.ru>
2025-11-20 13:43:32 +0300

    feat: upd libs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ const config = {
       return {
         name: 'docusaurus-tailwindcss',
         configurePostCss(postcssOptions) {
-          postcssOptions.plugins.push(require('tailwindcss'));
+          postcssOptions.plugins.push(require('@tailwindcss/postcss'));
           postcssOptions.plugins.push(require('autoprefixer'));
           return postcssOptions;
         },
